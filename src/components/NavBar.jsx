3fabbbc4ac8c2148db41301f83f6250063669cbc
Dor Levi <dorlevi7@gmail.com>
2025-12-02 04:28:04 +0200

    fix: add redirects file to handle React routes on refresh

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,7 +14,7 @@ function NavBar() {
   return (
     <nav className="navbar">
       <div className="navbar-logo" onClick={() => navigate("/home")}>
-        PokerApp
+        🃏 PokerApp
       </div>
 
       <div className="navbar-links">
